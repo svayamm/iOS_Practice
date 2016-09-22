2f0bb73553606a1043e74f47f1976f1e9b0faeb7
Svayam Mishra <svayamm@andrew.cmu.edu>
2016-09-22 00:05:38 -0400

    SimpleBrowser: Initial build, home url & view loading

--- SimpleBrowser/SimpleBrowser/ViewController.swift
+++ SimpleBrowser/SimpleBrowser/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var locationField: UITextField!
+    var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "https://www.google.com/")
+        webView.loadRequest(NSURLRequest(URL: url!))
+        webView.allowsBackForwardNavigationGestures = true
+        locationField.text = "www.google.com"
+        locationField.selectedTextRange = locationField.textRangeFromPosition(locationField.beginningOfDocument, toPosition: locationField.endOfDocument)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
